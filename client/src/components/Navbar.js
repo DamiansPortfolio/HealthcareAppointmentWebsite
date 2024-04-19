47d02336dc8dd2273cbfd2384c1bad8f6cccabc2
Damian Rozycki <drozyck1@students.towson.edu>
2024-04-19 13:41:06 -0400

    Installed react-icons and modified NavBar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,45 @@
 import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { IoMdMap } from "react-icons/io"; // This icon is for the directions link
 
 const NavBar = () => {
   return (
-    <Navbar bg="primary" variant="dark" expand="lg" sticky="top">
-      <Container>
-        <Navbar.Brand href="/">HealthcareSystem</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+    <>
+      {/* Top Navigation Bar */}
+      <Navbar bg="primary" variant="dark" expand="lg" sticky="top">
+        <Container>
+          <Navbar.Brand href="/">HealthcareSystem</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              {/* Add more Nav.Links as needed */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Bottom Navigation Bar */}
+      <Navbar fixed="bottom" bg="light" variant="light">
+        <Container>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/services">Services</Nav.Link>
+            <Nav.Link href="/locations">Locations</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
-            {/* Add more Nav.Links as needed */}
+            <Nav.Link href="/contact-us">Contact Us</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          <Nav>
+            <Nav.Link href="/directions">
+              <IoMdMap /> Directions
+            </Nav.Link>
+            <Button variant="outline-success" href="/book-appointment">
+              Book an Appointment
+            </Button>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
